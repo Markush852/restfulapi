238d1297c7e7a6ebe1f2f79259cd3ad21f0dec4f
Marcus <127418795+Markush852@users.noreply.github.com>
2023-03-27 16:50:19 -0300

    Implementación de EJS y sistema de ruteo.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const ejs = require('ejs');
-
 const app = express();
+const mainRoutes = require('./routes/mainRoutes');
+
+app.use('/' , mainRoutes)
+app.set( 'view engine' , 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
